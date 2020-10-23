5c87584a527bbe2e693bba0b27f8ee0e5fe71d7f
Manuel <mosquera.manuel2011@gmail.com>
2020-10-22 21:34:26 -0500

    formula para solucionar el sudoku finalizada

---
+++ solveSudoku.js
@@ -0,0 +1,256 @@
+tablero2 = {
+    '(0,0)': {'valor': "5", 'fila': '0', 'columna': '0'},
+    '(0,1)': {'valor': "", 'fila': '0', 'columna': '1'},
+    '(0,2)': {'valor': "", 'fila': '0', 'columna': '2'},
+    '(0,3)': {'valor': "", 'fila': '0', 'columna': '3'},
+    '(0,4)': {'valor': "7", 'fila': '0', 'columna': '4'},
+    '(0,5)': {'valor': "", 'fila': '0', 'columna': '5'},
+    '(0,6)': {'valor': "", 'fila': '0', 'columna': '6'},
+    '(0,7)': {'valor': "", 'fila': '0', 'columna': '7'},
+    '(0,8)': {'valor': "", 'fila': '0', 'columna': '8'},
+    '(1,0)': {'valor': "6", 'fila': '1', 'columna': '0'},
+    '(1,1)': {'valor': "", 'fila': '1', 'columna': '1'},
+    '(1,2)': {'valor': "", 'fila': '1', 'columna': '2'},
+    '(1,3)': {'valor': "1", 'fila': '1', 'columna': '3'},
+    '(1,4)': {'valor': "9", 'fila': '1', 'columna': '4'},
+    '(1,5)': {'valor': "5", 'fila': '1', 'columna': '5'},
+    '(1,6)': {'valor': "", 'fila': '1', 'columna': '6'},
+    '(1,7)': {'valor': "", 'fila': '1', 'columna': '7'},
+    '(1,8)': {'valor': "", 'fila': '1', 'columna': '8'},
+    '(2,0)': {'valor': "", 'fila': '2', 'columna': '0'},
+    '(2,1)': {'valor': "9", 'fila': '2', 'columna': '1'},
+    '(2,2)': {'valor': "8", 'fila': '2', 'columna': '2'},
+    '(2,3)': {'valor': "", 'fila': '2', 'columna': '3'},
+    '(2,4)': {'valor': "", 'fila': '2', 'columna': '4'},
+    '(2,5)': {'valor': "", 'fila': '2', 'columna': '5'},
+    '(2,6)': {'valor': "", 'fila': '2', 'columna': '6'},
+    '(2,7)': {'valor': "6", 'fila': '2', 'columna': '7'},
+    '(2,8)': {'valor': "", 'fila': '2', 'columna': '8'},
+    '(3,0)': {'valor': "8", 'fila': '3', 'columna': '0'},
+    '(3,1)': {'valor': "", 'fila': '3', 'columna': '1'},
+    '(3,2)': {'valor': "", 'fila': '3', 'columna': '2'},
+    '(3,3)': {'valor': "", 'fila': '3', 'columna': '3'},
+    '(3,4)': {'valor': "6", 'fila': '3', 'columna': '4'},
+    '(3,5)': {'valor': "", 'fila': '3', 'columna': '5'},
+    '(3,6)': {'valor': "", 'fila': '3', 'columna': '6'},
+    '(3,7)': {'valor': "", 'fila': '3', 'columna': '7'},
+    '(3,8)': {'valor': "3", 'fila': '3', 'columna': '8'},
+    '(4,0)': {'valor': "4", 'fila': '4', 'columna': '0'},
+    '(4,1)': {'valor': "", 'fila': '4', 'columna': '1'},
+    '(4,2)': {'valor': "", 'fila': '4', 'columna': '2'},
+    '(4,3)': {'valor': "8", 'fila': '4', 'columna': '3'},
+    '(4,4)': {'valor': "", 'fila': '4', 'columna': '4'},
+    '(4,5)': {'valor': "3", 'fila': '4', 'columna': '5'},
+    '(4,6)': {'valor': "", 'fila': '4', 'columna': '6'},
+    '(4,7)': {'valor': "", 'fila': '4', 'columna': '7'},
+    '(4,8)': {'valor': "1", 'fila': '4', 'columna': '8'},
+    '(5,0)': {'valor': "7", 'fila': '5', 'columna': '0'},
+    '(5,1)': {'valor': "", 'fila': '5', 'columna': '1'},
+    '(5,2)': {'valor': "", 'fila': '5', 'columna': '2'},
+    '(5,3)': {'valor': "", 'fila': '5', 'columna': '3'},
+    '(5,4)': {'valor': "2", 'fila': '5', 'columna': '4'},
+    '(5,5)': {'valor': "", 'fila': '5', 'columna': '5'},
+    '(5,6)': {'valor': "", 'fila': '5', 'columna': '6'},
+    '(5,7)': {'valor': "", 'fila': '5', 'columna': '7'},
+    '(5,8)': {'valor': "6", 'fila': '5', 'columna': '8'},
+    '(6,0)': {'valor': "", 'fila': '6', 'columna': '0'},
+    '(6,1)': {'valor': "6", 'fila': '6', 'columna': '1'},
+    '(6,2)': {'valor': "", 'fila': '6', 'columna': '2'},
+    '(6,3)': {'valor': "", 'fila': '6', 'columna': '3'},
+    '(6,4)': {'valor': "", 'fila': '6', 'columna': '4'},
+    '(6,5)': {'valor': "", 'fila': '6', 'columna': '5'},
+    '(6,6)': {'valor': "2", 'fila': '6', 'columna': '6'},
+    '(6,7)': {'valor': "8", 'fila': '6', 'columna': '7'},
+    '(6,8)': {'valor': "", 'fila': '6', 'columna': '8'},
+    '(7,0)': {'valor': "", 'fila': '7', 'columna': '0'},
+    '(7,1)': {'valor': "", 'fila': '7', 'columna': '1'},
+    '(7,2)': {'valor': "", 'fila': '7', 'columna': '2'},
+    '(7,3)': {'valor': "4", 'fila': '7', 'columna': '3'},
+    '(7,4)': {'valor': "1", 'fila': '7', 'columna': '4'},
+    '(7,5)': {'valor': "9", 'fila': '7', 'columna': '5'},
+    '(7,6)': {'valor': "", 'fila': '7', 'columna': '6'},
+    '(7,7)': {'valor': "", 'fila': '7', 'columna': '7'},
+    '(7,8)': {'valor': "5", 'fila': '7', 'columna': '8'},
+    '(8,0)': {'valor': "", 'fila': '8', 'columna': '0'},
+    '(8,1)': {'valor': "", 'fila': '8', 'columna': '1'},
+    '(8,2)': {'valor': "", 'fila': '8', 'columna': '2'},
+    '(8,3)': {'valor': "", 'fila': '8', 'columna': '3'},
+    '(8,4)': {'valor': "8", 'fila': '8', 'columna': '4'},
+    '(8,5)': {'valor': "", 'fila': '8', 'columna': '5'},
+    '(8,6)': {'valor': "", 'fila': '8', 'columna': '6'},
+    '(8,7)': {'valor': "7", 'fila': '8', 'columna': '7'},
+    '(8,8)': {'valor': "9", 'fila': '8', 'columna': '8'},
+}
+
+function crearCuadrantesFilasColumnas(tablero) {
+    let cuadrantes = {};
+    let filas = {};
+    let columnas = {};
+    // Crear cudrantes
+    //////////////////////////////////////////
+
+
+    // cuadrantes del sudoku:
+    //   1, 2, 3
+    //   4, 5, 6
+    //   7, 8, 9
+    // cuadranteF: Fila en la que se hace un cambio de cuadrante
+    // cuadranteC: Columna en la que se hace un cambio de cuadrante
+    let cuadranteF = 0
+    let cuadranteC = 1
+
+
+    // loop por cada  celda del tablero
+    for (key in tablero) {
+        const fila = tablero[key].fila
+        const columna = tablero[key].columna
+        const casilla = tablero[key].valor
+
+        // Revisar si la fila pasó las 3 filas correspondientes a un cuadrante
+        if (fila%3 == 0 && fila>0) {
+            // Sumar 3 cada vez se pasen 3 filas para designar el numero del cuadrante
+            cuadranteF = fila/3*3
+        }
+        // Revisar si la columna pasó las 3 columnas correspondientes a un cuadrante
+        if (columna%3 == 0) {
+            cuadranteC = columna/3 + 1
+        }
+        // Determinar numero del cuadrante 
+        let cuadrante = cuadranteF + cuadranteC
+        // Agregar la posicion de la celda al cuadrante correspondiente
+        if (!(cuadrante in cuadrantes)) {
+            cuadrantes[cuadrante] = {};
+            cuadrantes[cuadrante].valor = [];
+            cuadrantes[cuadrante].fila = [];
+            cuadrantes[cuadrante].columna = [];
+        }
+        cuadrantes[cuadrante].valor.push(casilla);
+        cuadrantes[cuadrante].fila.push(fila);
+        cuadrantes[cuadrante].columna.push(columna);
+
+    // Crear filas
+    //////////////////////////////////////////
+
+        // Revisar si la fila ya esta creada y si no crear la la lista para la fila
+        if (!(fila in filas)) {
+            filas[fila] = [];
+        } 
+        filas[fila].push(casilla);
+
+    // // Crear columnas
+    // //////////////////////////////////////////
+
+    //     // Revisar si la columna ya esta creada y si no crear la la lista para la columna
+        if (!(columna in columnas)) {
+            columnas[columna] = [];
+        } 
+        columnas[columna].push(casilla);
+    }
+    return [cuadrantes, columnas, filas]
+}
+
+function frequenciasPorNumero(tablero) {
+    // Iterar por cada celda del sudoku
+    const frequencias = {};
+    for (key in tablero) {
+        const numero = tablero[key].valor;
+        // Crear el numero en un objeto con las frequencias y agregar su conteo
+        if (!(numero in frequencias) && numero !== '') {
+            frequencias[numero] = 0;
+        }
+        if (numero !== '') {
+            frequencias[numero]++;
+        }
+    }
+    return frequencias;
+}
+
+function findMax(frequencias, evaluados) {
+    let maxVal = 0;
+    let maxNum;
+    let accumulator = 0;
+
+    for (key in frequencias) {
+        accumulator += parseInt(frequencias[key]);
+        if (frequencias[key] > maxVal && frequencias[key]<9 && !(evaluados.includes(key))) {
+            maxNum = key;
+            maxVal = frequencias[key];
+        }
+    }
+    // devolver 0 si ya se evaluaron todas las posibilidades y se debe reiniciar la evaluación
+    if (maxVal === 0) {
+        maxNum = 0;
+    }
+    // devolver -1 si el sudoku está resuelto
+    if (accumulator === 81) {
+        maxNum = -1;
+    }
+    return maxNum;
+}
+
+function solveSudoku(tablero) {
+    let estructuras = crearCuadrantesFilasColumnas(tablero);
+    let cuadrantes = estructuras[0];
+    let columnas = estructuras[1];
+    let filas = estructuras[2];
+
+    // Crear lista para llevar la revision de los numeros evaluados
+    let evaluados = []
+
+    let resuelto = false;
+    // Iterar hasta que el sudoku quede resuelto
+    while (resuelto === false) {
+        // buscar numero con mayor numero de frequencia
+        const frequencias = frequenciasPorNumero(tablero);
+        // Hayar el numero con la mayor frequencia para ser el numero con el que se va a buscar soluciones para el sudoku
+        let numToSearch = findMax(frequencias, evaluados);
+
+        // Reiniciar la lista de evaluados si ya se evaluó una ves todos los numeros posibles
+        if (numToSearch === 0) {
+            evaluados = [];
+            numToSearch = findMax(frequencias, evaluados);
+        }
+        // Salir de el loop si el sudoku está resuelto
+        if (numToSearch === -1) {
+            break;
+        }
+
+        // Iterar por cada cuadrante si el numero elegido aún no se encuentra en el
+        for (cuadrante in cuadrantes) {
+            // Lista para guardar las posibilidades del numero elegido en el cuadrante
+            const posib = [];
+
+            const valoresCuadrante = cuadrantes[cuadrante]['valor'];
+            if (!(valoresCuadrante.includes(numToSearch))) {
+                // Iterar por cada casilla en el cuadrante
+                for (let i=0; i < valoresCuadrante.length; i++) {
+                    const valor = valoresCuadrante[i];
+                    const fila = parseInt(cuadrantes[cuadrante]['fila'][i]);
+                    const columna = parseInt(cuadrantes[cuadrante]['columna'][i]);
+                    // Seleccionar alguna casilla que esté vacía del cuadrante
+                    if (valor === '') {
+                        // Agregar numero a posibilidades si el numero no está en la fila ni la columna aún
+                        if (!(filas[fila].includes(numToSearch)) && !(columnas[columna].includes(numToSearch))) {
+                            const ubicacion = `(${fila},${columna})`;
+                            posib.push(ubicacion);
+                        } 
+                    }
+                }
+                // Si el cuadrante solo tiene una ubicación posible para el numero elegido, agregar al tablero
+                if (posib.length === 1) {
+                    tablero[posib[0]]['valor'] = numToSearch;
+                    // actualizar valores de los cuadrantes, filas y columnas
+                    estructuras = crearCuadrantesFilasColumnas(tablero);
+                    cuadrantes = estructuras[0];
+                    columnas = estructuras[1];
+                    filas = estructuras[2];
+                }
+                // Agregar numero a lista de evaluados
+                evaluados.push(numToSearch);
+            }
+        }
+    }
+    return tablero
+}
+
+console.log(solveSudoku(tablero2));
